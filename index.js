54ca2290b7ab5842755e60138d2e0b0c3e7609c4
rsmsaurabh <rsm.saurabh@gmail.com>
2021-02-18 15:04:55 +0530

    updated file

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 module.exports = require('./dist/server')
+# Using yarn
+yarn add @sentry/react @sentry/tracing
+
+# Using npm
+npm install --save @sentry/react @sentry/tracing
+
+import * as Sentry from '@Sentry/browser';
+
+# Initialize with Data Source Name (dsn)
+Sentry.init({ dsn: 'dsn-string' });
